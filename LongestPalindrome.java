a793818c7d81ace83c3c966a3ce8a52034e55847
Jagadeesh Saravanan <jagadeesh.saravanan@oracle.com>
2025-07-17 08:11:43 -0700

    update and another solution to LongestPalindrome

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,8 +1,32 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 class Solution {
+
     public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Set<Character> set = new HashSet();
+        int count = 0;
+        for (char c: s.toCharArray()) {
+            if (!set.contains(c)) {
+                set.add(c);
+            } else {
+                count += 2;
+                set.remove(c);
+            }
+        }
+
+        return set.isEmpty() ? count : count + 1;
+
+    }
+
+
+    public int longestPalindromeUsingMap(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
@@ -12,20 +36,23 @@ class Solution {
             freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
 
-        int oneFreqPresent = 0;
+        boolean oneFreqPresent = false;
         int count = 0;
         for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
-            
+            char c = entry.getKey();
             int freq = entry.getValue();
 
             if (freq % 2 == 1) {
-                oneFreqPresent++;
-            } 
+                oneFreqPresent = true;
+                count += freq - 1;
+            } else {
+                count += freq;
+            }
             
-            count+= (freq/2) * 2;
+            
             
         }
 
-        return oneFreqPresent > 0 ? count + 1: count;
+        return oneFreqPresent ? count + 1: count;
     }
 }
